79a084e65bca52d7443f88620776e751a26b249e
Scott Salhanick <ssalhanick@gmail.com>
2026-03-02 12:21:50 -0600

    adding python configuration and a requirements file for easier replication

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Root directory of the project
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Neo4j
+NEO4J_URI      = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+# CourtListener
+COURTLISTENER_TOKEN = os.getenv("COURTLISTENER_TOKEN")
+
+# Landmark Fourth Amendment case IDs
+LANDMARK_IDS = [
+    "terry-v-ohio-1968",
+    "katz-v-united-states-1967",
+    "mapp-v-ohio-1961",
+    "united-states-v-leon-1984",
+    "illinois-v-gates-1983"
+]
